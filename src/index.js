5cfa72b6a30e4e00e65fbe898eb9dc405c73126d
mesutkoc <mestkoc34@gmail.com>
2021-10-16 00:02:06 +0300

    update indexjs

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { CityProvider } from "./contexts/CityContext";
+import { CitynameProvider } from "./contexts/CitynameContext";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://graphql-weather-api.herokuapp.com/",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <ApolloProvider client={client}>
+    <CityProvider>
+      <CitynameProvider>
+        <App />
+      </CitynameProvider>
+    </CityProvider>
+  </ApolloProvider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
